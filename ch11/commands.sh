c739ffb7573066ebd1b14bdccf8f8f9510b4fd49
Gates Wang <gateswang2012@gmail.com>
2026-02-16 18:38:28 -0500

    create way to merge

--- ch11/commands.sh
+++ ch11/commands.sh
@@ -28,6 +28,14 @@ kubectl apply -f ch11/configmanagementplugins/kustomize-helm-app.yaml
 # test if yq avail
 kubectl exec -n argocd -it deployments/argocd-repo-server -c kustomize-helm-plugin -- yq --version
 
-# inspect image
-kubectl get pod  argocd-repo-server-65b4c8fdd-7qk8t -o jsonpath='{.spec.containers[*].image}'
-# quay.io/argoproj/argocd:v2.12.6 quay.io/argoproj/argocd:v3.3.0
+# test locally
+cd /Users/gateswang/Programming/Learning/devops/k8/argocd/argocd-up-and-running-book/ch11/configmanagementplugins  
+export ARGOCD_ENV_RELEASE_STAGE=dev
+export ARGOCD_APP_PARAMETER_REPLICA_COUNT=3
+export ARGOCD_APP_PARAMETER_ENVIRONMENT=prod
+export ENV_CONTENT=$(env | grep "^ARGOCD_ENV_" | sed 's/^ARGOCD_ENV_//' | awk -F= '{printf "  %s: %s\n", $1, $2}')
+export PARAM_CONTENT=$(env | grep "^ARGOCD_APP_PARAMETER_" | sed 's/^ARGOCD_APP_PARAMETER_//' | awk -F= '{printf "  %s: %s\n", $1, $2}')
+
+kustomize build --enable-helm > base.yaml
+yq -n '.data.env = (strenv(ENV_CONTENT) | from_yaml) | .data.params = (strenv(PARAM_CONTENT) | from_yaml)' > input.yaml
+yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' base.yaml input.yaml > merged.yaml
