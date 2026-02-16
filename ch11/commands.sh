e0d02d7acf918695b7720f2648460f5a6832c2c0
Gates Wang <gateswang2012@gmail.com>
2026-02-16 17:54:50 -0500

    disable dynamic injection

--- ch11/commands.sh
+++ ch11/commands.sh
@@ -21,6 +21,7 @@ kubectl get cm kustomize-helm -n kustomize-helm -oyaml
 # changes to CMP
 kubectl apply -f ch11/configmanagementplugins/kustomize-helm-plugin.yml
 kubectl -n argocd rollout restart deployment argocd-repo-server
+kubectl -n argocd rollout status deployment argocd-repo-server
 kubectl rollout pause deployment/argocd-repo-server
 kubectl apply -f ch11/configmanagementplugins/kustomize-helm-app.yaml
 
